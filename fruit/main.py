3c0a4838bedf1fc4f94e2ab31276e7f080ebba73
Tim Couper <drtimcouper@gmail.com>
2013-11-12 15:01:22 +0000

    Using Screenmanager with transitions

--- fruit/main.py
+++ fruit/main.py
@@ -10,9 +10,14 @@ from kivy.loader import Loader
 from kivy.uix.image import Image
 from kivy.uix.floatlayout import FloatLayout
 from kivy.graphics import Color, Rectangle
+from kivy.uix.screenmanager import ScreenManager, Screen
+from kivy.uix.screenmanager import SlideTransition
+from kivy.uix.screenmanager import SwapTransition
+from kivy.uix.screenmanager import WipeTransition
+from kivy.uix.screenmanager import FadeTransition
 
 
-class FruitScreen(FloatLayout):
+class FruitScreen(Screen):
 
     def __init__(self, **kwargs):
         super(FruitScreen, self).__init__(**kwargs)
@@ -25,33 +30,29 @@ class FruitScreen(FloatLayout):
         sainsburys = self.load_image(pos_hint={'center_x': .7, 'center_y':.9},
                                                         size_hint=(.3,0))
         self.add_widget(sainsburys)
+        self.name = self.__class__.__name__
 
     # replace with .kv file
     def _update_rect(self, instance, value):
       self.rect.pos= instance.pos
       self.rect.size = instance.size
 
-    def _image_loaded(self, proxy_image):
-        if proxy_image.image.texture:
-            self.image.texture = proxy_image.texture
-
     def load_image(self, **kwargs):
         this_dir = os.path.dirname(__file__)
         jpg = os.path.join(this_dir,'sainsbury_s.jpeg')
         return Image(source=jpg, **kwargs)
-        # proxy_image = Loader.image(jpg)
-        # proxy_image.bind(on_load=self._image_loaded)
-        # return Image()
 
 
 class FruitMainScreen(FruitScreen):
-   def __init__(self, **kwargs):
+    def __init__(self, **kwargs):
+
         super(FruitMainScreen, self).__init__(**kwargs)
 
         btn = Button(text='Get products', size_hint=(.3,.1),
                              pos_hint={'center_x': .7, 'center_y':.7},
                              background_color=[238,238,0,1],
-                             color=[0,0,0,1])
+                             color=[0,0,0,1],
+                             on_release=self.on_release)
         self.add_widget(btn)
 
         label = Label(text='Welcome to the thing',
@@ -60,19 +61,25 @@ class FruitMainScreen(FruitScreen):
                               color=[0,0,0,1])
         self.add_widget(label)
 
+    def on_release(self, event):
+        print self.manager.current
+        self.manager.current = self.manager.next()
+        print self.manager.current
+
+
+class FruitListScreen(FruitScreen):
+    def __init__(self, **kwargs):
+        super(FruitListScreen, self).__init__(**kwargs)
 
 
 class Fruit(App):
     def build(self):
-        return FruitMainScreen()
+        root = ScreenManager(transition=WipeTransition())
+        root.add_widget(FruitMainScreen())
+        root.add_widget(FruitListScreen())
+        return root
 
-    # def on_touch_move(self, touch):
-    #     print(touch.profile)
-    #     return super(Peops, self).on_touch_move(touch)
 
-    # def on_touch_down(self, touch):
-    #     print(touch.profile)
-    #     return super(Peops, self).on_touch_move(touch)
 
 if __name__ == '__main__':
     Fruit().run()
