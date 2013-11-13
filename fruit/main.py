101a10f2dea106a02e7d7d0721cca89347df9d1a
Tim Couper <drtimcouper@gmail.com>
2013-11-13 13:30:44 +0000

    FadeTransition

--- fruit/main.py
+++ fruit/main.py
@@ -1,4 +1,5 @@
 import os
+import weakref
 
 import kivy
 kivy.require('1.7.2')
@@ -10,11 +11,34 @@ from kivy.loader import Loader
 from kivy.uix.image import Image
 from kivy.uix.floatlayout import FloatLayout
 from kivy.graphics import Color, Rectangle
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.scrollview import ScrollView
+from kivy.properties import ListProperty
+from kivy.uix.checkbox import CheckBox
 from kivy.uix.screenmanager import ScreenManager, Screen
-from kivy.uix.screenmanager import SlideTransition
-from kivy.uix.screenmanager import SwapTransition
-from kivy.uix.screenmanager import WipeTransition
 from kivy.uix.screenmanager import FadeTransition
+
+
+PAGE_BACKGROUND_COLOR=[.7,.8,1,1]
+BUTTON_COLOR = [2,.95,.2,1]
+BLACK = [0,0,0,1]
+
+STORAGE={'apples':  {'price':1.99, 'description': 'nice and juicy'},
+                    'oranges': {'description': 'round and tea-mobile'},
+                    'pears': {},
+                    'bananas': {},
+                    'strawberries': {},
+                    'pineapples': {},
+                    'lychees' :{},
+                    'guavas': {},
+                    'raspberries': {},
+                    'mangoes': {},
+                    'clementines':{},
+                    'satsumas': {},
+                    'pomegranates': {},
+
+                    }
 
 
 class FruitScreen(Screen):
@@ -23,11 +47,11 @@ class FruitScreen(Screen):
         super(FruitScreen, self).__init__(**kwargs)
         # to put into .kv file:
         with self.canvas.before:
-            Color(.7,.8,1,1)
+            Color(*PAGE_BACKGROUND_COLOR)
             self.rect=Rectangle(size=self.size, pos=self.pos)
             self.bind(size=self._update_rect, pos=self._update_rect)
 
-        sainsburys = self.load_image(pos_hint={'center_x': .7, 'center_y':.9},
+        sainsburys = self.load_image(pos_hint={'center_x': .5, 'center_y':.9},
                                                         size_hint=(.3,0))
         self.add_widget(sainsburys)
         self.name = self.__class__.__name__
@@ -48,37 +72,110 @@ class FruitMainScreen(FruitScreen):
 
         super(FruitMainScreen, self).__init__(**kwargs)
 
-        btn = Button(text='Get products', size_hint=(.3,.1),
-                             pos_hint={'center_x': .7, 'center_y':.7},
-                             background_color=[238,238,0,1],
-                             color=[0,0,0,1],
+        btn = Button(text='List products', size_hint=(.28,.08),
+                             pos_hint={'center_x': .5, 'center_y':.5},
+                             background_color=BUTTON_COLOR,
+                             color=BLACK,
                              on_release=self.on_release)
         self.add_widget(btn)
 
-        label = Label(text='Welcome to the thing',
-                              pos_hint={'center_x': .4, 'center_y':.45},
-                              font_size=50,
-                              color=[0,0,0,1])
-        self.add_widget(label)
 
     def on_release(self, event):
-        print self.manager.current
         self.manager.current = self.manager.next()
-        print self.manager.current
 
 
 class FruitListScreen(FruitScreen):
     def __init__(self, **kwargs):
         super(FruitListScreen, self).__init__(**kwargs)
 
+        btn = Button(text='back', size_hint=(.15,.07),
+                             pos_hint={'center_x': .2, 'center_y':.9},
+                             background_color=BUTTON_COLOR,
+                             color=BLACK,
+                             on_release=self.on_release)
+        self.add_widget(btn)
+
+        list_widget = FruitListWidget(size_hint=(1, .8),
+                                                pos_hint = {'center_x': .5, 'center_y':.4})
+        self.add_widget(list_widget)
+
+    def on_release(self, event):
+        self.manager.current = self.manager.previous()
+
+
+class FruitDetailScreen(FruitScreen):
+    fruit_name='unset'
+    def __init__(self, **kwargs):
+        super(FruitDetailScreen, self).__init__(**kwargs)
+
+        btn = Button(text='back', size_hint=(.15,.07),
+                             pos_hint={'center_x': .2, 'center_y':.9},
+                             background_color=BUTTON_COLOR,
+                             color=BLACK,
+                             on_release=self.on_release)
+
+        self.add_widget(btn)
+        self.slate = FloatLayout(size_hint=(1, .8))
+        self.add_widget(self.slate)
+
+    def on_enter(self):
+        details = Label(text=self.fruit_name)
+        self.slate.add_widget(details)
+
+    def on_leave(self):
+        self.slate.clear_widgets()
+        self.fruit_name='unset'
+
+    def on_release(self, event):
+        self.manager.current = self.manager.previous()
+
+
+class FruitListWidget(ScrollView):
+
+    def __init__(self, **kwargs):
+        super(FruitListWidget, self).__init__(**kwargs)
+        self.build_grid()
+
+    def get_names(self):
+        return sorted(STORAGE)
+
+    def build_grid(self):
+
+        grid = BoxLayout(orientation='vertical')
+        #grid.bind(minimum_height=grid.setter('height'))
+        for name in self.get_names():
+            btn = FruitNameButton(text=name,
+                                                  color=BLACK,
+                                                  background_color=[2,2,2,1],
+                                                  text_size=(700,None),
+                                                  )
+            grid.add_widget(btn)
+
+        self.add_widget(grid)
+
+
+class FruitNameButton(Button):
+    pressed = ListProperty([0, 0])
+
+    def on_touch_down(self, touch):
+        if self.collide_point(*touch.pos):
+            self.pressed = touch.pos
+
+        super(FruitNameButton, self).on_touch_down(touch)
+
+    def on_pressed(self, instance, pos):
+        screen_manager = self.parent.parent.parent.manager
+        screen_manager.current = screen_manager.next()
+        screen_manager.current_screen.fruit_name = instance.text
+
 
 class Fruit(App):
     def build(self):
-        root = ScreenManager(transition=WipeTransition())
+        root = ScreenManager(transition=FadeTransition())
         root.add_widget(FruitMainScreen())
         root.add_widget(FruitListScreen())
+        root.add_widget(FruitDetailScreen())
         return root
-
 
 
 if __name__ == '__main__':
